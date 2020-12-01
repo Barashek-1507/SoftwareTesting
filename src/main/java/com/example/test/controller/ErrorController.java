900ed517c228bc363b43ff7aadef1021f25c283a
valeria_ergasheva <maybach@MacBook-Pro-5.local>
2020-12-02 00:50:49 +0300

    Add errors conroller

---
+++ src/main/java/com/example/test/controller/ErrorController.java
@@ -0,0 +1,37 @@
+package org.baggio.sweater.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
+    /**
+     * @deprecated
+     */
+    @Override
+    public String getErrorPath() {
+        return null;
+    }
+
+    @GetMapping("/error")
+    public String handleError(HttpServletRequest request, Map<String, Object> map){
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        if (status != null) {
+            int statusCode = Integer.parseInt(status.toString());
+
+            if(statusCode == HttpStatus.NOT_FOUND.value()) {
+                map.put("message", "404 Error\nBad request");
+            }
+            else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                map.put("message", "505 Error\nErrors in the server");
+            }
+        }
+        return "error";
+    }
+
+}
